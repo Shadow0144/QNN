21cfe565ba68148ca45bdb7da671fcbcfc89d962
Shadow0144 <CorbinCogswell@gmail.com>
2020-09-03 20:52:09 +0900

    Created a basic working program

---
+++ QNN/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.QNN
+{
+    class QNN
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                // Run Hello World and wait for finish
+                HelloQ.Run(qsim).Wait();
+            }
+        }
+    }
+}
--- QNN/Program.qs
+++ QNN/Program.qs
@@ -1,11 +1,11 @@
-﻿namespace Quantum.QNN {
-
+﻿namespace Quantum.QNN
+{
     open Microsoft.Quantum.Canon;
     open Microsoft.Quantum.Intrinsic;
-
     
     @EntryPoint()
-    operation HelloQ () : Unit {
+    operation HelloQ () : Unit 
+    {
         Message("Hello quantum world!");
     }
 }
